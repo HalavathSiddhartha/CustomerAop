ae8efa7ad703ef789feabfd81e274f88b48b4291
Halavath Siddhartha <79624557+HalavathSiddhartha@users.noreply.github.com>
2023-10-12 23:40:35 +0530

    Update Customer.java

--- CustomerApp/src/Customer.java
+++ CustomerApp/src/Customer.java
@@ -1,76 +1,89 @@
-class Customer {
-	private int customerId;
-	private String customerName;
-	private String customerEmail;
-	private String customerPassword;
-	private String customerAddress;
-	private String customerPhoneNo;
+import java.util.List;
 
-	public Customer(int customerId, String customerName, String customerEmail, String customerPassword,
-			String customerAddress, String customerPhoneNo) {
-		this.customerId = customerId;
-		this.customerName = customerName;
-		this.customerEmail = customerEmail;
-		this.customerPassword = customerPassword;
-		this.customerAddress = customerAddress;
-		this.customerPhoneNo = customerPhoneNo;
-	}
+public class Customer {
+    private int customerId;
+    private String customerName;
+    private String customerEmail;
+    private String customerPassword;
+    private String customerAddress;
+    private String customerPhoneNo;
+    private List<String> services;  // List to store the services
 
-	public int getCustomerId() {
-		return customerId;
-	}
+    public Customer(int customerId, String customerName, String customerEmail, String customerPassword,
+                    String customerAddress, String customerPhoneNo, List<String> services) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPassword = customerPassword;
+        this.customerAddress = customerAddress;
+        this.customerPhoneNo = customerPhoneNo;
+        this.services = services;  // Initialize the services list
+    }
 
-	public void setCustomerId(int customerId) {
-		this.customerId = customerId;
-	}
+    public int getCustomerId() {
+        return customerId;
+    }
 
-	public String getCustomerName() {
-		return customerName;
-	}
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
 
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
+    public String getCustomerName() {
+        return customerName;
+    }
 
-	public String getCustomerEmail() {
-		return customerEmail;
-	}
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 
-	public void setCustomerEmail(String customerEmail) {
-		this.customerEmail = customerEmail;
-	}
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
 
-	public String getCustomerPassword() {
-		return customerPassword;
-	}
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
 
-	public void setCustomerPassword(String customerPassword) {
-		this.customerPassword = customerPassword;
-	}
+    public String getCustomerPassword() {
+        return customerPassword;
+    }
 
-	public String getCustomerAddress() {
-		return customerAddress;
-	}
+    public void setCustomerPassword(String customerPassword) {
+        this.customerPassword = customerPassword;
+    }
 
-	public void setCustomerAddress(String customerAddress) {
-		this.customerAddress = customerAddress;
-	}
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
 
-	public String getCustomerPhoneNo() {
-		return customerPhoneNo;
-	}
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
 
-	public void setCustomerPhoneNo(String customerPhoneNo) {
-		this.customerPhoneNo = customerPhoneNo;
-	}
+    public String getCustomerPhoneNo() {
+        return customerPhoneNo;
+    }
 
-	@Override
-	public String toString() {
-		return "Customer ID: " + customerId +
-				"\nName: " + customerName +
-				"\nEmail: " + customerEmail +
-				"\nPassword: " + customerPassword +
-				"\nAddress: " + customerAddress +
-				"\nPhone Number: " + customerPhoneNo + "\n";
-	}
+    public void setCustomerPhoneNo(String customerPhoneNo) {
+        this.customerPhoneNo = customerPhoneNo;
+    }
+
+    public List<String> getServices() {
+        return services;
+    }
+
+    public void setServices(List<String> services) {
+        this.services = services;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer ID: " + customerId +
+                "\nName: " + customerName +
+                "\nEmail: " + customerEmail +
+                "\nPassword: " + customerPassword +
+                "\nAddress: " + customerAddress +
+                "\nPhone Number: " + customerPhoneNo +
+                "\nServices: " + services + "\n";
+    }
 }
